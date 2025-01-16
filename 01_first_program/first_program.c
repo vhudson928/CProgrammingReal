429e57bf2c9b54593380d2fa79aceb07d10df930
vhudson928 <vhudson9197@floridapoly.edu>
2025-01-16 16:30:28 +0000

    Fixed a bug in word game scoring logic

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,19 @@
+//One-line comment
+//This is the first program which prints a message
+
+/*
+
+Multiline Comment.
+
+*/
+ 
+#include <stdio.h>
+
+//Create a main function.
+int main() {
+    printf("Welcome\nto\nCop2271.\n");
+    printf("Coding\nTime.\n");
+
+return 0;
+
+}
